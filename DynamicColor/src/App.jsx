37abce21b5c14877d93377134f7358d2901cbf13
RahulJingar <rjingarnsd@gmail.com>
2025-05-13 22:41:07 +0530

    initial commit

--- DynamicColor/src/App.jsx
+++ DynamicColor/src/App.jsx
@@ -1,259 +1,376 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
 
-  const cartStyle = {
-        border:'1px solid #cccccc3b',
-        width:'200px',
-        boxShadow: '1px 2px 3px 0px #cccccc57',
-        margin:'10px',
-        margin:'30px'
+  const [cardStyle, setCardStyle] = useState(
+    {
+      border:'1px solid #cccccc33b',
+      width:'200px',
+      boxShadow:'1px 2px 3px 0px #cccccc57',
+      margin:'10px',
+      marginLeft:"70px"
+    }
+  )
+
+  const [textColor, serTextColor] = useState('black')
+
+  const [grid, setGrid] = useState(true);
+
+  const updateTheme = (bgColor, textColor) => {
+    setCardStyle({...cardStyle, backgroundColor:bgColor})
+    serTextColor(textColor)
   }
 
   return (
-    <>
-        <h1
+    <div>
+      <h1
+      style={{
+        color:'red',
+        marginLeft:'505px'
+      }}
+      >
+        Dynamic and Conditional Inline Style
+      </h1>
+
+
+      <div style={{
+        marginLeft:'600px'
+      }}>
+        
+      <button 
         style={{
-          color:'red',
-          fontSize:'35px',
-          marginLeft:'550px'
+          border:'2px solid green',
+          padding:'10px',
+          backgroundColor:'black',
+          color:'white'
         }}
-        ><b>Inline Style in React Js</b></h1>
-    <div
-    style={{
-      display:'flex',
-      flexWrap:'wrap',
-      gap:'50px'
-    }}
-    >
+      onClick={()=>updateTheme('gray','red')}>Grey Theme</button>
+      <button
+       style={{
+          border:'2px solid green',
+          padding:'10px',
+          backgroundColor:'black',
+          color:'white'
+        }}
+      onClick={()=>updateTheme('white','black')}>Default Theme</button>
+      <button
+       style={{
+          border:'2px solid green',
+          padding:'10px',
+          backgroundColor:'black',
+          color:'white'
+        }}
+      onClick={()=>setGrid(!grid)}>Toggle Grid</button>
+      </div>
 
-      <div style={cartStyle}>
+      <div
+      style={{
+        // display:'grid' ? 'block': 'flex'
+        display:grid ? 'block' : 'flex',
+         flexWrap:'wrap'
+      }}
+      >
+         <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+        <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+       <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+        <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+        <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+       <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+        <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+        <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+       <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+        <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+        <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+       <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+        <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+        <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+       <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+        <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+        <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+       <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+        <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+        <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+       <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+          <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+          <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
 
-       <div style={cartStyle}>
+
+          <div style={cardStyle}>
         <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
+        <div style={{ color:textColor}}>
+          <h3
+        style={{marginLeft:'50px'}}
+        >Rahul Jingar</h3>
+
+        <p style={{marginLeft:'30px'}}><b>Full Stack Developer</b></p>
+        </div>
+      </div>
+
+
+          <div style={cardStyle}>
+        <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
+        <div style={{ color:textColor}}>
+          <h3
         style={{marginLeft:'50px'}}
         >Rahul Jingar</h3>
 
         <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+        </div>
       </div>
-
-      <div style={cartStyle}>
-        <img src="https://img.freepik.com/premium-vector/old-gentleman_24908-26037.jpg" alt="" width='200px'/>
-        <h3
-        style={{marginLeft:'50px'}}
-        >Rahul Jingar</h3>
-
-        <p style={{marginLeft:'20px'}}><b>Full Stack Developer</b></p>
+    
       </div>
-      
-      </div>
-    </>
+    </div>
   )
 }
 
