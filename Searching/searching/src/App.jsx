9ead776e2c4ae4f45c3c31a0736ebb621e6f6946
Cpp <rjingarnsd@gmail.com>
2025-08-05 11:21:26 +0530

    Update App.jsx

--- Searching/searching/src/App.jsx
+++ Searching/searching/src/App.jsx
@@ -4,49 +4,59 @@ const App = () => {
 
   const [search,setSearch] = useState('');
 
-   const users = [
-    { id: 1, name: "Aarav", lastName: "Sharma", number: "9876543101" },
-    { id: 2, name: "Vihaan", lastName: "Mehra", number: "9876543102" },
-    { id: 3, name: "Aditya", lastName: "Verma", number: "9876543103" },
-    { id: 4, name: "Vivaan", lastName: "Malhotra", number: "9876543104" },
-    { id: 5, name: "Arjun", lastName: "Singh", number: "9876543105" },
-    { id: 6, name: "Sai", lastName: "Reddy", number: "9876543106" },
-    { id: 7, name: "Krishna", lastName: "Nair", number: "9876543107" },
-    { id: 8, name: "Ishaan", lastName: "Kapoor", number: "9876543108" },
-    { id: 9, name: "Kartik", lastName: "Deshmukh", number: "9876543109" },
-    { id: 10, name: "Reyansh", lastName: "Joshi", number: "9876543110" },
+  const data = [
+    {id: 1, name: "rahul",lastName: "jingar",number: 7297998875},
+    {id: 2, name: "yashu",lastName: "sharma",number: 9928057119},
+    {id: 3, name: "jitesh",lastName: "dabi",number: 7297997485},
+    {id: 4, name: "arun",lastName: "sankhla",number: 4152639874},
+    {id: 9, name: "vivek",lastName: "sisodiya",number: 7297995157},
+    {id: 6, name: "sudarshan",lastName: "udawat",number: 7852696366},
+    {id: 7, name: "priansh",lastName: "rastogi",number: 7285412875},
+    {id: 8, name: "divya",lastName: "rochani",number: 7545455875},
+    {id: 5, name: "rahul",lastName: "nama",number: 7745154875},
+    {id: 10, name: "saloni",lastName: "kumari",number: 7275457875},
+    {id: 11, name: "krishna",lastName: "soni",number: 7245484875},
   ];
 
-  const filterdUser = users.filter(item=>{
-    const fullName = `${item.name} ${item.lastName} ${item.number}`
-    return fullName.toLowerCase().includes(search);
+  const filterdUser = data.filter((item)=>{
+    const fullName = `${item.id} ${item.name} ${item.lastName} ${item.number}`
+   return fullName.toLowerCase().includes(search);
   })
 
-  return (
-    <div>
+  const user = filterdUser.sort((a,b) => a.id-b.id)
 
-    <input 
-    type="text" 
-    placeholder='Search bar'
-    value={search}
-    onChange={(e)=>setSearch(e.target.value)}
-    />
+  console.log(`>>>>>>>>user>>`,user)
+
+
+
+
+  return (
+  <div>
+
+    <div>
+      <input 
+      type="text" 
+      placeholder='Search Bar'
+      value={search}
+      onChange={(e)=>setSearch(e.target.value)}
+      />
+    </div>
 
       <div>
-        {
-          filterdUser.length > 0 ? (
-            filterdUser.map(item=>(
-          <div>
-            <p>{`${item.name} ${item.lastName}`}</p>
-            <p>{`${item.number}`}</p>
-          </div>
-        ))
-          ) : (
-            <p>No Data Found</p>
-          )
-        }
-      </div>
+      {filterdUser.length > 0 ? 
+      (
+        filterdUser.map((item)=>(
+        <div>
+          <p>{`${item.name} ${item.lastName}`}</p>
+          <p>{`${item.number}`}</p>
+        </div>
+      ))
+      ) : (
+        <p>Data Not Found</p>
+      ) 
+    }
     </div>
+  </div>
   )
 }
 
